366c5979bda624b2efda0269ec5d68b8d3ca27e9
Sebastian Wrede <swrede@techfak.uni-bielefeld.de>
2011-02-22 20:09:09 +0000

    added ctr, cleaned up code

--- src/rsb/Publisher.java
+++ src/rsb/Publisher.java
@@ -1,12 +1,29 @@
+/**
+ * ============================================================
+ *
+ * This file is a part of the RSBJava project
+ *
+ * Copyright (C) 2010 CoR-Lab, Bielefeld University
+ *
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation;
+ * either version 2, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * ============================================================
+ */
 package rsb;
 
-import java.util.UUID;
 import java.util.logging.Logger;
 
 import rsb.transport.Router;
 import rsb.transport.TransportFactory;
-import rsb.util.Timestamp;
-
 
 /**
  * This class offers a method to send messages to any registered Subscribers.
@@ -26,17 +43,13 @@ public class Publisher implements RSBObject {
 	private final static Logger log = Logger.getLogger(Publisher.class.getName()); 
 
 	/* publisher's name within it's scope */
-	protected String name;
-	
-	/* receiver scope URI */
-//	protected XcfUri receiverUri;
-	
-//	PublisherInterceptDistributor interceptManager;
-	
+	protected String uri;
+
+	/* state variable for publisher instance */
 	protected PublisherState state;
 
 	/* transport factory object */
-//	protected TransportFactory transportFactory;
+	protected TransportFactory transportFactory;
 	
 	/* transport router */
 	protected Router router;
@@ -66,45 +79,36 @@ public class Publisher implements RSBObject {
 		}
 
 		protected RSBEvent send(RSBEvent e) {		
-			e.setUri(name);
+			e.setUri(uri);
 			e.ensureID();
-			// TODO move event creation into factory?
-		//	e.getMetadata().setSenderUri(uri);
-		//	e.getMetadata().setReceiverUri(receiverUri);
-			Timestamp now = new Timestamp();
-			//e.getMetadata().setCreatedAt(now);
-			//e.getMetadata().setUpdatedAt(now);
 			router.publishSync(e);
 			return e;
 		}
 		
 	}
-	
-	public Publisher(String name, TransportFactory tfac) {
+
+	private void initMembers(String u, TransportFactory tfac) {
 		state = new PublisherStateInactive(this);
-		this.name = name;
-//		manager = m;
-//		uri = new XcfUri(Configurator.getInstance().getInterfaceLocalUri(name));
-//		this.transportFactory = tfac;
-		router = new Router(tfac);
-		// find the root scope
-		// TO DO this has to be set in a better way from configuration
-//		XcfUri current = uri.getScope();
-//		while(current.getParentScope() != null) {
-//			current = current.getParentScope();
-//		}
-//		receiverUri = current;
-		// TODO what about multiple targets?
-		//receiverUri = new XcfUri(Configurator.getInstance().getFirstInterfaceRemoteUri(name));
-		log.fine("New publisher instance with URI " + name + " created"); //+ uri.getUri());
+		this.transportFactory = tfac;
+		this.uri = u;
+		router = new Router(transportFactory);
+		log.info("New publisher instance created: [URI:" + uri + ",State: Inactive"); 		
+	}
+	
+	public Publisher(String u) {		 
+		initMembers(u, TransportFactory.getInstance());
+	}	
+	
+	public Publisher(String u, TransportFactory tfac) {
+		initMembers(u,tfac);
 	}
 
 	/**
-	 * Returns name of the publisher
-	 * @return name of Publisher as a String
+	 * Returns URI of the publisher
+	 * @return URI of Publisher as a String
 	 */
-	public String getName() {
-		return name;
+	public String getURI() {
+		return uri;
 	}
 	
 	public synchronized void activate() throws InitializeException {
@@ -116,7 +120,7 @@ public class Publisher implements RSBObject {
 	}
 	
 	/**
-	 * Send a message to all subscribed participants. 
+	 * Send an RSBEvent to all subscribed participants 
 	 */
 	public synchronized void send(RSBEvent e) {
 		state.send(e);
