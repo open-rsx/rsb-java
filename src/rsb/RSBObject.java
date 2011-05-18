a8aeebfe4188975ff44b04f4d7ae88eef2553ca5
Johannes Wienke <jwienke@techfak.uni-bielefeld.de>
2011-05-18 16:08:53 +0000

    comment fixing

--- src/rsb/RSBObject.java
+++ src/rsb/RSBObject.java
@@ -22,19 +22,19 @@ package rsb;
 
 /**
  * @author swrede
- *
+ * 
  */
 public interface RSBObject {
-	
-	/*
-	 * Activates all network resources that belong to
-	 * a specific object.
+
+	/**
+	 * Activates all network resources that belong to a specific object.
 	 */
 	public void activate() throws RSBException;
-	
-	/*
-	 * Deactivate all network resources that are owned
-	 * by a specific object in order to reactivate it.
+
+	/**
+	 * Deactivate all network resources that are owned by a specific object in
+	 * order to reactivate it.
 	 */
 	public void deactivate() throws RSBException;
+
 }
