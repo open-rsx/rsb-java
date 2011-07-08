9ad5a9a180966a66b98211835b4c692004a64832
Sebastian Wrede <swrede@techfak.uni-bielefeld.de>
2011-07-08 17:10:43 +0000

    fix for some PMD warnings, implemented hashCode

--- src/rsb/converter/ConverterSignature.java
+++ src/rsb/converter/ConverterSignature.java
@@ -1,12 +1,18 @@
 package rsb.converter;
 
-
+/**
+ * Immutable object representing a converter signature. Used to index the
+ * converter maps.
+ * 
+ * @author swrede
+ *
+ */
 public class ConverterSignature {
 	
 	/** RSB WireSchema */
-	private String schema;
+	private final String schema;
 	/** Java Type */
-	private String datatype;
+	private final String datatype;
 
 	public ConverterSignature(String schema, String datatype) {
 		this.schema = schema;
@@ -21,14 +27,50 @@ public class ConverterSignature {
 		return datatype;
 	}
 	
+	/* (non-Javadoc)
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result
+				+ ((datatype == null) ? 0 : datatype.hashCode());
+		result = prime * result + ((schema == null) ? 0 : schema.hashCode());
+		return result;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
 	@Override
 	public boolean equals(Object obj) {
-		if (obj instanceof ConverterSignature) {
-			ConverterSignature other = (ConverterSignature) obj;
-			if (other.getDatatype().contentEquals(this.getDatatype()) && other.getSchema().contentEquals(this.getSchema())) {
-				return true;
-			}
+		if (this == obj) {
+			return true;
 		}
-		return false;
+		if (obj == null) {
+			return false;
+		}
+		if (!(obj instanceof ConverterSignature)) {
+			return false;
+		}
+		ConverterSignature other = (ConverterSignature) obj;
+		if (datatype == null) {
+			if (other.datatype != null) {
+				return false;
+			}
+		} else if (!datatype.equals(other.datatype)) {
+			return false;
+		}
+		if (schema == null) {
+			if (other.schema != null) {
+				return false;
+			}
+		} else if (!schema.equals(other.schema)) {
+			return false;
+		}
+		return true;
 	}	
+	
+	
 }
