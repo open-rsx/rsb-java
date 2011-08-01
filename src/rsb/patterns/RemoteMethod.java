d05c8819fa98be2627b8b02e2c54f8ccca0f9963
Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
2011-08-01 22:34:33 +0000

    Added RemoteMethod class in src/rsb/patterns/RemoteMethod.java refs #438 * src/rsb/patterns/RemoteMethod.java: new class RemoteMethod;   represents a remote method and implements its invocation

---
+++ src/rsb/patterns/RemoteMethod.java
@@ -0,0 +1,26 @@
+package rsb.patterns;
+
+/**
+ * Objects of this class represent methods provided by a remote
+ * server.
+ *
+ * @author jmoringe
+ */
+public class RemoteMethod<U, T> extends Method {
+    /**
+     * Create a new RemoteMethod object that represent the remote
+     * method named @a name provided by @a server.
+     *
+     * @param server
+     *            The remote server providing the method.
+     * @param name
+     *            The name of the method.
+     */
+    public RemoteMethod(Server server, String name) {
+	super(server, name);
+    }
+
+    public U call(T request) {
+	return (U) null;
+    }
+};
