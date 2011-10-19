e58972d21868a4d7edfb6100148a13446e5bb4bc
Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
2011-10-19 19:52:54 +0000

    Added license header in src/util/Properties.java * src/util/Properties.java: added license header

--- src/rsb/util/Properties.java
+++ src/rsb/util/Properties.java
@@ -1,3 +1,21 @@
+/* ============================================================
+ *
+ * This file is part of the RSB project.
+ *
+ * Copyright (C) 2011 Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * ============================================================  */
+
 package rsb.util;
 
 import java.io.FileInputStream;
