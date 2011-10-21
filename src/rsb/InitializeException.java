7eb8093f619e003c6802edf67155a8f3d1d33f44
Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
2011-10-21 22:26:06 +0000

    Improved comments in src/rsb/InitializeException.java * src/rsb/InitializeException.java: added license header; removed   irrelevant parts of the documentation string for InitializeException

--- src/rsb/InitializeException.java
+++ src/rsb/InitializeException.java
@@ -1,33 +1,45 @@
 /**
- * 
+ * ============================================================
+ *
+ * This file is a part of the RSBJava project
+ *
+ * Copyright (C) 2010, 2011 CoR-Lab, Bielefeld University
+ *
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation;
+ * either version 2, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * ============================================================
  */
 package rsb;
 
-
-
 /**
  * An InitializeException indicates erroneous situations during
- * the setup of the communication infrastructure. Usually, either 
- * the dispatcher nameservice can not be accessed, because it is
- * not running or the XCF.Initial.Host property is set to an incorrect
- * value. Other sources for this exception are related to the spread
- * group communication framework. Its properties can be changed via the
- * Spread.Host / Spread.Port and Spread.StartDaemon properties. Please 
- * inspect the value of getMessage() in order to check for the actual 
- * reason for this exception. 
- * 
+ * the setup of the communication infrastructure.
+ *
+ * Other sources for this exception are related to the spread group
+ * communication framework. The value of getMessage() can be inspected
+ * to determine the actual reason for this exception.
+ *
  * @author swrede
  *
  */
 public class InitializeException extends RSBException {
 
 	/**
-	 * 
+	 *
 	 */
 	private static final long serialVersionUID = -5223250815059688771L;
 
 	/**
-	 * 
+	 *
 	 */
 	public InitializeException() {
 	}
@@ -53,5 +65,5 @@ public class InitializeException extends RSBException {
 	public InitializeException(String message, Throwable cause) {
 		super(message, cause);
 	}
-	
+
 }
