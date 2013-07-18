672af55c05eb19481b03158d44acdee699a9107d
Johannes Wienke <jwienke@techfak.uni-bielefeld.de>
2013-07-18 20:24:24 +0200

    Made connector tests generic.
    Encapsulated the generic connector tests already available for spread in
    abstract base classes so that they can be reused for the socket
    transport.

--- test/rsb/transport/spread/SpreadRoundtripTest.java
+++ test/rsb/transport/spread/SpreadRoundtripTest.java
@@ -27,30 +27,23 @@
  */
 package rsb.transport.spread;
 
-import static org.junit.Assert.assertEquals;
-
 import java.nio.ByteBuffer;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
-import java.util.List;
 
-import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 import org.junit.runners.Parameterized.Parameters;
 
-import rsb.Event;
-import rsb.EventId;
-import rsb.ParticipantId;
 import rsb.QualityOfServiceSpec;
 import rsb.QualityOfServiceSpec.Ordering;
 import rsb.QualityOfServiceSpec.Reliability;
-import rsb.Scope;
 import rsb.Utilities;
 import rsb.converter.StringConverter;
 import rsb.converter.UnambiguousConverterMap;
-import rsb.transport.EventHandler;
+import rsb.transport.ConnectorRoundtripCheck;
+import rsb.transport.InPushConnector;
+import rsb.transport.OutConnector;
 
 /**
  * Test for {@link SpreadOutConnector}.
@@ -58,12 +51,10 @@ import rsb.transport.EventHandler;
  * @author jwienke
  */
 @RunWith(value = Parameterized.class)
-public class SpreadRoundtripTest {
-
-    private final int size;
+public class SpreadRoundtripTest extends ConnectorRoundtripCheck {
 
     public SpreadRoundtripTest(final int size) {
-        this.size = size;
+        super(size);
     }
 
     @Parameters
@@ -73,74 +64,25 @@ public class SpreadRoundtripTest {
         return Arrays.asList(data);
     }
 
-    @Test(timeout = 4000)
-    public void roundtrip() throws Throwable {
-
-        final Scope scope = new Scope("/a/test/scope");
-
+    @Override
+    protected OutConnector createOutConnector() throws Throwable {
         final SpreadWrapper outWrapper = Utilities.createSpreadWrapper();
-        final UnambiguousConverterMap<ByteBuffer> inStrategy = new UnambiguousConverterMap<ByteBuffer>();
-        inStrategy.addConverter("utf-8-string", new StringConverter());
         final UnambiguousConverterMap<ByteBuffer> outStrategy = new UnambiguousConverterMap<ByteBuffer>();
         outStrategy.addConverter(String.class.getName(), new StringConverter());
         final SpreadOutConnector outPort = new SpreadOutConnector(outWrapper,
                 outStrategy, new QualityOfServiceSpec(Ordering.ORDERED,
                         Reliability.RELIABLE));
+        return outPort;
+    }
 
-        final List<Event> receivedEvents = new ArrayList<Event>();
+    @Override
+    protected InPushConnector createInConnector() throws Throwable {
+        final UnambiguousConverterMap<ByteBuffer> inStrategy = new UnambiguousConverterMap<ByteBuffer>();
+        inStrategy.addConverter("utf-8-string", new StringConverter());
         final SpreadWrapper inWrapper = Utilities.createSpreadWrapper();
         final SpreadInPushConnector inPort = new SpreadInPushConnector(
                 inWrapper, inStrategy);
-        inPort.addHandler(new EventHandler() {
-
-            @Override
-            public void handle(final Event event) {
-                synchronized (receivedEvents) {
-                    receivedEvents.add(event);
-                    receivedEvents.notifyAll();
-                }
-            }
-
-        });
-
-        inPort.setScope(scope);
-        inPort.activate();
-        outPort.setScope(scope);
-        outPort.activate();
-
-        final StringBuilder builder = new StringBuilder();
-        for (int i = 0; i < this.size; ++i) {
-            builder.append('c');
-        }
-
-        final Event event = new Event(scope, String.class, builder.toString());
-        event.setId(new ParticipantId(), 42);
-        event.getMetaData().setUserInfo("foo", "a long string");
-        event.getMetaData().setUserInfo("barbar", "a long string again");
-        event.getMetaData().setUserTime("asdasd", 324234);
-        event.getMetaData().setUserTime("xxx", 42);
-        event.addCause(new EventId(new ParticipantId(), 23434));
-        event.addCause(new EventId(new ParticipantId(), 42));
-
-        outPort.push(event);
-
-        synchronized (receivedEvents) {
-            while (receivedEvents.size() != 1) {
-                receivedEvents.wait();
-            }
-
-            final Event receivedEvent = receivedEvents.get(0);
-
-            // normalize times as they are not important for this test
-            event.getMetaData().setReceiveTime(
-                    receivedEvent.getMetaData().getReceiveTime());
-
-            assertEquals(event, receivedEvent);
-        }
-
-        inPort.deactivate();
-        outPort.deactivate();
-
+        return inPort;
     }
 
 }
