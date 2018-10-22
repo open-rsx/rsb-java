87cbde949bf9296941fa10e502a0b718cb95e5bb
Johannes Wienke <jwienke@techfak.uni-bielefeld.de>
2018-10-22 16:58:54 +0200

    Prevent errors in case a callback returns null
    Proactively handle returned null values from a method callback.
    
    This will fix https://code.cor-lab.org/issues/2773

---
+++ rsb-java/src/test/java/rsb/patterns/LocalMethodTest.java
@@ -0,0 +1,76 @@
+/**
+ * ============================================================
+ *
+ * This file is part of the rsb-java project
+ *
+ * Copyright (C) 2018 CoR-Lab, Bielefeld University
+ *
+ * This file may be licensed under the terms of the
+ * GNU Lesser General Public License Version 3 (the ``LGPL''),
+ * or (at your option) any later version.
+ *
+ * Software distributed under the License is distributed
+ * on an ``AS IS'' basis, WITHOUT WARRANTY OF ANY KIND, either
+ * express or implied. See the LGPL for the specific language
+ * governing rights and limitations.
+ *
+ * You should have received a copy of the LGPL along with this
+ * program. If not, go to http://www.gnu.org/licenses/lgpl.html
+ * or write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ * The development of this software was supported by:
+ *   CoR-Lab, Research Institute for Cognition and Robotics
+ *     Bielefeld University
+ *
+ * ============================================================
+ */
+package rsb.patterns;
+
+import org.junit.Test;
+
+import rsb.Event;
+import rsb.EventId;
+import rsb.Factory;
+import rsb.LoggingEnabled;
+import rsb.ParticipantCreateArgs;
+import rsb.ParticipantId;
+import rsb.Scope;
+
+/**
+ * @author jwienke
+ */
+public class LocalMethodTest extends LoggingEnabled {
+
+    @Test
+    public void testHandleNullReply() throws Exception {
+
+        final Callback callback = new Callback() {
+
+            @Override
+            public Event internalInvoke(final Event request)
+                    throws UserCodeException {
+                return null;
+            }
+
+        };
+
+        final LocalMethod method = new LocalMethod(
+                new ParticipantCreateArgs<ParticipantCreateArgs<?>>() {
+                    // dummy type
+                }.setScope(new Scope("/test")).setConfig(
+                    Factory.getInstance().getDefaultParticipantConfig()),
+                callback);
+        method.activate();
+
+        try {
+            final Event dummyRequest = new Event();
+            dummyRequest.setId(new EventId(new ParticipantId(), 0));
+            // must not throw an exception despite the callback returning null
+            method.internalNotify(dummyRequest);
+        } finally {
+            method.deactivate();
+        }
+    }
+
+}
